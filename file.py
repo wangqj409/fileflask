219904f96be2677cebb635bff98a5984594149d0
wangqj409 <53250934+wangqj409@users.noreply.github.com>
2024-03-20 20:52:12 +0800

    Update file.py

--- file.py
+++ file.py
@@ -4,30 +4,28 @@ import uuid
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 200 * 1000 * 1000
+# app.config['MAX_CONTENT_LENGTH'] = 200 * 1000 * 1000
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-def allowed_file(filename):
+def allowedfile(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def genewname():
     return datetime.now().strftime("%Y/%m/%d/") + str(uuid.uuid4()).replace('-', '')
 
-@app.route("/")
-def hello_world():
-    return ["<p>Hello, World!</p>"]
-
 
 @app.route("/upload", methods=["POST"])
 def upload():
     file1 = request.files['file']
-    if not allowed_file(file1.filename):
+    if not allowedfile(file1.filename):
         return {'path':'', 'error':'not allowed file'}
 
     dst = "static/" + genewname()
     dp = Path(dst)
     if not dp.parent.exists():
         dp.parent.mkdir(parents=True)
+        
     file1.save(dst)
+
     return {'path':dst, 'error':''}
