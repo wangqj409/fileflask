e4bc03f39ab2f68f7aa4f7d175c5c3aa0522e5a8
wangqj409 <53250934+wangqj409@users.noreply.github.com>
2024-03-21 18:05:36 +0800

    Update file.py

--- file.py
+++ file.py
@@ -1,8 +1,10 @@
-from flask import Flask,request
+from flask import Flask, request
 from pathlib import Path
 import uuid
 from datetime import datetime
-import os,sys
+import os
+import pandas as pd
+from collections import defaultdict
 
 
 apihost = os.getenv('API_HOST')
@@ -29,7 +31,23 @@ def upload():
     dp = Path(dst)
     if not dp.parent.exists():
         dp.parent.mkdir(parents=True)
-        
+
     file1.save(dst)
 
     return {'path':apihost + '/' + dst, 'error':''}
+
+@app.route("/parsexcel", methods=["POST"])
+def parsexcel():
+    filepath = request.files['filepath']
+    r = pd.read_excel(filepath, na_values='')
+    # return {'data':r.to_dict(), 'error':''}
+
+    result = defaultdict()
+    for i in r:
+        for j in range(len(r[i])):
+            result[j] = result[j] if j in result else {}
+            result[j][i] = f'{r[i][j]}'
+    ret = []
+    for i in result:
+        ret.append( result[i])
+    return {'data':ret, 'error':''}
